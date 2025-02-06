c235c5491c979c7e47393578e562d95789f4d368
Minhazul Abedin <minhazulabedin44@gmail.com>
2025-02-06 19:13:26 +0600

    auth and setup of tab done

--- app/(auth)/(tabs)/feed/index.tsx
+++ app/(auth)/(tabs)/feed/index.tsx
@@ -1,10 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import { useAuth } from '@clerk/clerk-expo'
 
 const Feed = () => {
+    const { signOut } = useAuth()
     return (
         <View>
             <Text>Feed</Text>
+            <TouchableOpacity onPress={() => signOut()}><Text>Logout</Text></TouchableOpacity>
         </View>
     )
 }
